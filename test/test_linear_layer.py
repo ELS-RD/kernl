3464bb7f0cf396c959fda484a2d35b95119e8ff7
Michaël Benesty <pommedeterresautee@users.noreply.github.com>
2022-08-10 10:46:04 +0200

    feat: add cuda graph support (#3)
    * feat: add cuda graph support
    
    * fix: update to new benchmark API

--- test/test_linear_layer.py
+++ test/test_linear_layer.py
@@ -1,10 +1,12 @@
 import torch
 import pytest
+
+from implementations.cuda_graph import CudaGraph
 from implementations.linear_layer import linear_layer
 
 @pytest.mark.parametrize("size", [128 * i for i in range(2, 10)])
 @pytest.mark.parametrize("batch", [8])
-@pytest.mark.parametrize("implementation", ["cublas", "triton"])
+@pytest.mark.parametrize("implementation", ["cublas", "triton", "triton_cuda_graph", "pytorch"])
 def test_benchmark(benchmark, size, batch, implementation):
     torch.manual_seed(0)
 
@@ -17,12 +19,17 @@ def test_benchmark(benchmark, size, batch, implementation):
 
     torch_linear_layer = torch.nn.Linear(K, K*4, bias=False, device="cuda", dtype=torch.float16)
     torch_linear_layer.weight.data = layer_weight
+    expected = torch_linear_layer(a)
 
     if implementation == "cublas":
-        benchmark(torch_linear_layer, a)
-    if implementation == "triton":
-        benchmark(linear_layer, x=a, weight=layer_weight, bias=None)
-
-    value, _ = linear_layer(x=a, weight=layer_weight, bias=None)
-    expected = torch_linear_layer(a)
+        value = benchmark(torch_linear_layer, a)
+    elif implementation == "triton":
+        value, _ = benchmark(linear_layer, x=a, weight=layer_weight, bias=None)
+    elif implementation == "triton_cuda_graph":
+        cg = CudaGraph(weights=layer_weight)
+        value = benchmark(cg.run, inputs=a)
+    elif implementation == "pytorch":
+        value = benchmark(torch_linear_layer, input=a)
+    else:
+        raise ValueError(f"Unknown implementation {implementation}")
     assert torch.allclose(value, expected)
