fix: masking on add

test/test_debugger.py
@@ -5,13 +5,13 @@ from utils.debugger import TritonDebugger
 torch.random.manual_seed(123)
 
 
-def test_launch():
-    vec_len = 256
-    block_size = 64
+def test_add():
+    vec_len = 250
+    block_size = 64  # not a vec len multiple to test masks
     x = torch.rand(vec_len)
     y = torch.rand_like(x)
     o = torch.zeros_like(x)
-    tl = TritonDebugger([vec_len // block_size], inputs=[x, y, o])
+    tl = TritonDebugger([TritonDebugger.cdiv(vec_len, block_size)], inputs=[x, y, o])
 
     def add_kernel(
             x_ptr,  # *Pointer* to first input vector
@@ -50,3 +50,47 @@ def test_launch():
             BLOCK_SIZE=block_size,
         )
     assert torch.allclose(o, x + y)
+
+
+def test_softmax():
+    vec_len = 250
+    x = torch.rand((16, vec_len))
+    o = torch.zeros_like(x)
+    block_size = 256  # not a vec len multiple to test masks
+    tl = TritonDebugger([TritonDebugger.cdiv(vec_len, block_size)], inputs=[x, o])
+
+    def softmax_kernel(
+            output_ptr, input_ptr, input_row_stride, output_row_stride, n_cols,
+            BLOCK_SIZE: tl.constexpr
+    ):
+        # The rows of the softmax are independent, so we parallelize across those
+        row_idx = tl.program_id(0)
+        # The stride represents how much we need to increase the pointer to advance 1 row
+        row_start_ptr = input_ptr + row_idx * input_row_stride
+        # The block size is the next power of two greater than n_cols, so we can fit each
+        # row in a single block
+        col_offsets = tl.arange(0, BLOCK_SIZE)
+        input_ptrs = row_start_ptr + col_offsets
+        # Load the row into SRAM, using a mask since BLOCK_SIZE may be > than n_cols
+        row = tl.load(input_ptrs, mask=col_offsets < n_cols, other=-float('inf'))
+        # Substract maximum for numerical stability
+        row_minus_max = row - tl.max(row, axis=0)
+        # Note that exponentials in Triton are fast but approximate (i.e., think __expf in CUDA)
+        numerator = tl.exp(row_minus_max)
+        denominator = tl.sum(numerator, axis=0)
+        softmax_output = numerator / denominator
+        # Write back output to DRAM
+        output_row_start_ptr = output_ptr + row_idx * output_row_stride
+        output_ptrs = output_row_start_ptr + col_offsets
+        tl.store(output_ptrs, softmax_output, mask=col_offsets < n_cols)
+
+    while tl.has_next():
+        softmax_kernel(
+            output_ptr=tl.tensor_ptr[o],
+            input_ptr=tl.tensor_ptr[x],
+            input_row_stride=x.stride(0),
+            output_row_stride=o.stride(0),
+            n_cols=vec_len,
+            BLOCK_SIZE=block_size,
+        )
+    assert torch.allclose(o, torch.softmax(x, dim=1)), f"{o} != {torch.softmax(x, dim=1)}"

utils/debugger.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import torch
 
 
@@ -40,40 +42,81 @@ class TritonDebugger:
 
         d = dict()
         for t in inputs:
-            d[(previous_boundary, previous_boundary+t.nelement())] = t
-            previous_boundary = previous_boundary+t.nelement()
+            d[(previous_boundary, previous_boundary + t.nelement())] = t
+            previous_boundary = previous_boundary + t.nelement()
 
         self.inputs = RangeKeyDict(d)
         self.tensor_ptr: dict[torch.Tensor, int] = {v: k[0] for k, v in d.items()}
 
-    def program_id(self, axis: int) -> int:
+    def program_id(self, axis: int) -> torch.Tensor:
         if self.current_grid_position[axis] > self.grid[axis]:
             raise Exception("Program id out of bounds")
 
         current_position = self.current_grid_position[axis]
         self.current_grid_position[axis] += 1
-        return current_position
+        return torch.tensor(current_position)
 
     def has_next(self) -> bool:
         return self.current_grid_position[0] < self.grid[0]
 
-    def arange(self, start: int, end: int) -> torch.Tensor:
+    @staticmethod
+    def arange(start: int, end: int) -> torch.Tensor:
+        """
+        Returns contiguous values within the open interval [start, end).
+        @param start: start of the interval
+        @param end: end of the interval. Must be a power of two >= start.
+        @return: a tensor of size (end - start) with values in [start, end).
+        """
+        assert (end & (end-1) == 0) and end != 0, "end must be a power of 2"
+        assert start < end, "start must be less than end"
         return torch.arange(start=start, end=end)
 
-    def load(self, ptr: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
-        first_elem_ptr = ptr.data[0].item()
-        t = self.inputs[first_elem_ptr]
-        first_ptr = self.tensor_ptr[t]
-        ptr = ptr - first_ptr
-        mask_index, = (mask == True).nonzero(as_tuple=True)
-        ptr = ptr[mask_index]
-        return t[ptr]
+    def load(self, ptr: torch.Tensor, mask: Optional[torch.Tensor] = None, other: Optional[float] = None) -> torch.Tensor:
+        first_elem_ptr = ptr.flatten()[0].item()
+        tensor = self.inputs[first_elem_ptr]
+        first_ptr = self.tensor_ptr[tensor]
+        tensor_flat = tensor.flatten()
+        assert tensor_flat.data_ptr() == tensor.data_ptr()
+        indexes = ptr - first_ptr
+        if mask is not None:
+            if other is None:
+                indexes = indexes[mask]
+                return tensor_flat[indexes]
+            else:
+                block = torch.full_like(ptr, fill_value=other, dtype=tensor_flat.dtype)
+                indexes = indexes[mask]
+                data = tensor_flat[indexes]
+                selection = [slice(0, s) for s in indexes.size()]
+                block[selection] = data
+                return block
 
     def store(self, ptr: torch.Tensor, data: torch.Tensor, mask: torch.Tensor) -> None:
-        first_elem_ptr = ptr.data[0].item()
+        first_elem_ptr = ptr.flatten()[0].item()
         t = self.inputs[first_elem_ptr]
         first_ptr = self.tensor_ptr[t]
         ptr = ptr - first_ptr
-        mask_index, = (mask == True).nonzero(as_tuple=True)
-        ptr = ptr[mask_index]
-        t[ptr] = data
+        ptr = ptr[mask]
+        t_flat = t.flatten()
+        assert t_flat.data_ptr() == t.data_ptr()
+        t_flat[ptr] = data
+        t_flat.resize_(t.shape)
+
+    @staticmethod
+    def cdiv(x: int, y: int) -> int:
+        """
+        Ceiling division returns the closest integer greater than or equal to the quotient.
+        """
+        return (x + y - 1) // y
+
+    @staticmethod
+    def max(x: torch.Tensor, axis=0) -> torch.Tensor:
+        return torch.max(x, dim=axis).values
+
+    @staticmethod
+    def exp(x: torch.Tensor) -> torch.Tensor:
+        return torch.exp(x)
+
+    @staticmethod
+    def sum(x: torch.Tensor, axis=0) -> torch.Tensor:
+        return torch.sum(x, dim=axis)
+