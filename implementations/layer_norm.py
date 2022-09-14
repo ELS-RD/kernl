2cedbfa1f6cb0a2fa20d3096e68057033f43b4ac
pommedeterresautee <pommedeterresautee@msn.com>
2022-09-14 20:31:32 +0200

    fix: following review comments

--- implementations/layer_norm.py
+++ implementations/layer_norm.py
@@ -14,57 +14,71 @@ def _layer_norm_fwd_fused_single_pass(
         A,
         Weight,
         Bias,
-        Mean, Rstd,
+        Mean, std,
         stride, N, eps,
         BLOCK_SIZE: tl.constexpr,
 ):
     """
-    Based on Welford's variance computation algorithm.
+    Layernorm based on Welford's variance computation algorithm.
     https://changyaochen.github.io/welford/
     https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance
+
+    :param Out: output tensor
+    :param A: input tensor
+    :param Weight: weights applied to the normalized input
+    :param Bias: bias added to the normalized input
+    :param Mean: save mean tensor for backward
+    :param std: save standard deviation tensor for backward
+    :param stride: stride of the input tensor
+    :param N: number of elements per row in the input tensor
+    :param eps: epsilon value to avoid division by zero
+    :param BLOCK_SIZE: number of threads per block
+    :return: None
     """
     # position of elements processed by this program
-    row = tl.program_id(0)
-    Out += row * stride
-    A += row * stride
+    _idx = tl.program_id(0)
+    out_ptr = Out + _idx * stride
+    a_ptr = A + _idx * stride
     # compute mean
     mean = 0.0
     var = 0.0
-    for start in range(0, N, BLOCK_SIZE):
-        end = min((start + BLOCK_SIZE), N)
-        nb_block_col = end - start
-        cols = start + tl.arange(0, BLOCK_SIZE)
-        mask = cols < N
-        a = tl.load(A + cols, mask=mask, other=0., eviction_policy="evict_last").to(tl.float32)
+    for start_n_offset in range(0, N, BLOCK_SIZE):
+        end_n_offset = min((start_n_offset + BLOCK_SIZE), N)
+        nb_block_cols = end_n_offset - start_n_offset
+        column_offset = start_n_offset + tl.arange(0, BLOCK_SIZE)
+        mask = column_offset < N
+        # eviction policy below have little impact now because of new implementation. Kept as is.
+        a = tl.load(a_ptr + column_offset, mask=mask, other=0., eviction_policy="evict_last").to(tl.float32)
 
-        block_mean = tl.sum(a, axis=0) / nb_block_col
+        block_mean = tl.sum(a, axis=0) / nb_block_cols
         # mean is 0 or has a mask applied to it, no need to mask delta_mean!
         delta_mean = block_mean - mean
         delta_mean_sqr = delta_mean * delta_mean
 
         block_delta = tl.sum((a - block_mean) * a, axis=0)
         # mean has a mask!
-        mean += tl.sum((a - mean) * mask, axis=0) / end
-        var += block_delta + delta_mean_sqr * (start * nb_block_col) / end
+        mean += tl.sum((a - mean) * mask, axis=0) / end_n_offset
+        var += block_delta + delta_mean_sqr * (start_n_offset * nb_block_cols) / end_n_offset
 
     var = var / N
     rstd = 1 / tl.sqrt(var + eps)
 
-    # write-back mean/rstd
-    tl.store(Mean + row, mean)
-    tl.store(Rstd + row, rstd)
+    # write-back mean/rstd for backward pass
+    tl.store(Mean + _idx, mean)
+    tl.store(std + _idx, rstd)
 
     # multiply by weight and add bias
     for off in range(0, N, BLOCK_SIZE):
-        cols = off + tl.arange(0, BLOCK_SIZE)
-        mask = cols < N
-        weight = tl.load(Weight + cols, mask=mask)
-        bias = tl.load(Bias + cols, mask=mask)
-        a = tl.load(A + cols, mask=mask, other=0., eviction_policy="evict_first").to(tl.float32)
+        column_offset = off + tl.arange(0, BLOCK_SIZE)
+        mask = column_offset < N
+        weight = tl.load(Weight + column_offset, mask=mask)
+        bias = tl.load(Bias + column_offset, mask=mask)
+        # eviction policy helps to keep weights in cache (reused by other threads)
+        a = tl.load(a_ptr + column_offset, mask=mask, other=0., eviction_policy="evict_first").to(tl.float32)
         a_hat = (a - mean) * rstd
         out = a_hat * weight + bias
         # write-back
-        tl.store(Out + cols, out, mask=mask)
+        tl.store(out_ptr + column_offset, out, mask=mask)
 
 
 @triton.jit
@@ -120,8 +134,9 @@ def layer_norm_forward(a: torch.Tensor, weight: torch.Tensor, bias: torch.Tensor
     # reshape input data into 2D tensor
     a_arg = a.reshape(-1, a.shape[-1])
     M, N = a_arg.shape
+    # tensors below for backward pass
     mean = torch.empty((M,), dtype=torch.float32, device="cuda")
-    rstd = torch.empty((M,), dtype=torch.float32, device="cuda")
+    std = torch.empty((M,), dtype=torch.float32, device="cuda")
     # Less than 64KB per feature: enqueue fused kernel
     MAX_FUSED_SIZE = 65536 // a.element_size()
     BLOCK_SIZE = min(MAX_FUSED_SIZE, triton.next_power_of_2(N))
@@ -135,7 +150,7 @@ def layer_norm_forward(a: torch.Tensor, weight: torch.Tensor, bias: torch.Tensor
         a_arg,
         weight,
         bias,
-        mean, rstd,
+        mean, std,
         a_arg.stride(0), N, eps,
         BLOCK_SIZE=BLOCK_SIZE,
         num_warps=num_warps,
