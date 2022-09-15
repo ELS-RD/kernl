7f3aefae7fc299c00097fb41ea6749cdef4d8311
pommedeterresautee <pommedeterresautee@msn.com>
2022-09-15 09:02:09 +0200

    fix: add manual seed

--- test/test_torchdynamo_bert.py
+++ test/test_torchdynamo_bert.py
@@ -75,10 +75,12 @@ def test_benchmark_implementations(benchmark, model_reference_fp32, shape: (int,
 def test_support_shape_change(model_reference_fp32):
     """Test that the model can handle shape changes without being reloaded/rebuilt."""
     for name, implementation in implementations.items():
+        torch.manual_seed(0)
         model_tested = implementation.model()
         for shape in [(1, 64), (8, 256), (16, 256), (16, 64)]:
             pytorch_input = get_input_causal(shape) if implementation.is_causal else get_input_non_causal(shape)
             expected = model_reference_fp32(**pytorch_input)
             result = model_tested(**pytorch_input)
+            max_diff = torch.max(torch.abs(result["last_hidden_state"].float() - expected["last_hidden_state"]))
             assert torch.allclose(result["last_hidden_state"].float(), expected["last_hidden_state"],
-                                  atol=1e-1), f"failed on {name} with shape {shape}"
+                                  atol=1e-1), f"[{name}] failed with shape {shape}, max diff: {max_diff}"
