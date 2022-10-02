327d27518a03d6a8583a84d2e00060a4646b43ff
pommedeterresautee <pommedeterresautee@msn.com>
2022-10-02 13:58:35 +0200

    feat: add apache 2 license

--- test/models/ort_utils.py
+++ test/models/ort_utils.py
@@ -1,6 +1,21 @@
 """
 All the tooling to ease ONNX Runtime usage.
 """
+#  Copyright 2022 Lefebvre Sarrut
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
 import ctypes as C
 from ctypes.util import find_library
 from typing import Dict, Optional, Tuple
