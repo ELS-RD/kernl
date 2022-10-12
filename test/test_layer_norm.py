972ae30be516cf7858cfde09c35ae4dbe4141771
Michaël Benesty <pommedeterresautee@users.noreply.github.com>
2022-10-12 13:39:03 +0200

    feat: add rmsnorm kernel for T5 (#90)
    * feat: add rmsprop baseline
    
    * feat: triton implementation + benchmarks
    
    * fix: add missing doc

--- test/test_layer_norm.py
+++ test/test_layer_norm.py
@@ -25,18 +25,21 @@ from kernl.implementations.layer_norm import (
     layer_norm,
     layer_norm_xformers,
     pytorch_naive_layernorm,
+    pytorch_naive_rmsnorm,
 )
 
 
-implementations = {
+implementations_layer_norm = {
     "pytorch": lambda weight, bias, eps: lambda x: torch.nn.functional.layer_norm(x, weight.shape, weight, bias, eps),
     "triton_original": lambda weight, bias, eps: lambda x: layer_norm(
-        x, weight, bias, eps, _layer_norm_fwd_fused_multi_pass
+        x, weight, bias, eps, _layer_norm_fwd_fused_multi_pass, use_rms_norm=False
     ),
     "triton_improved": lambda weight, bias, eps: lambda x: layer_norm(
-        x, weight, bias, eps, _layer_norm_fwd_fused_single_pass
+        x, weight, bias, eps, _layer_norm_fwd_fused_single_pass, use_rms_norm=False
     ),
-    "triton_xformer": lambda weight, bias, eps: lambda x: layer_norm(x, weight, bias, eps, layer_norm_xformers),
+    "triton_xformer": lambda weight, bias, eps: lambda x: layer_norm(
+        x, weight, bias, eps, layer_norm_xformers, use_rms_norm=False
+    ),
     "pytorch_naive": lambda weight, bias, eps: lambda x: pytorch_naive_layernorm(x, weight, bias, eps),
 }
 
@@ -45,7 +48,7 @@ implementations = {
 @pytest.mark.parametrize("shape", [128, 512, 1024, 2048, 4096], ids=lambda x: f"shape={x}x{x}")
 @pytest.mark.parametrize("cuda_graphs", [True, False], ids=["cuda_graphs", "no_cuda_graphs"])
 @pytest.mark.parametrize("dtype", [torch.float16, torch.float32], ids=["fp16", "fp32"])
-@pytest.mark.parametrize("implementation", implementations.keys())
+@pytest.mark.parametrize("implementation", implementations_layer_norm.keys())
 def test_benchmark_layer_norm(benchmark, shape: int, dtype, cuda_graphs: bool, implementation: str):
     M = N = shape
     eps = 1e-5
@@ -60,7 +63,41 @@ def test_benchmark_layer_norm(benchmark, shape: int, dtype, cuda_graphs: bool, i
     layer_bias = layer_bias.to(dtype)
     x = x.to(dtype)
 
-    fn = implementations[implementation](layer_weight, layer_bias, eps)
+    fn = implementations_layer_norm[implementation](layer_weight, layer_bias, eps)
+    if cuda_graphs:
+        run = cuda_graphs_wrapper(model=fn, inputs=[x], copy_outputs=False)
+        # cuda graphs wraps output in a tuple
+        fn = lambda tensor: run(tensor)[0]  # noqa: E731
+
+    value = benchmark(fn, x)
+    check_all_close(value.float(), expected, atol=1e-1)
+
+
+implementations_rms_norm = {
+    "pytorch": lambda weight, eps: lambda x: pytorch_naive_rmsnorm(x, weight, eps),
+    "triton_improved": lambda weight, eps: lambda x: layer_norm(
+        x, weight, None, eps, _layer_norm_fwd_fused_single_pass, use_rms_norm=True
+    ),
+}
+
+
+@pytest.mark.parametrize("cuda_graphs", [True, False], ids=["cuda_graphs", "no_cuda_graphs"])
+@pytest.mark.parametrize("dtype", [torch.float16, torch.float32], ids=["fp16", "fp32"])
+@pytest.mark.parametrize("shape", [128, 512, 1024, 2048, 4096], ids=lambda x: f"shape={x}x{x}")
+@pytest.mark.parametrize("implementation", implementations_rms_norm.keys())
+def test_benchmark_rms_norm(benchmark, shape: int, dtype, cuda_graphs: bool, implementation: str):
+    M = N = shape
+    eps = 1e-5
+    factory_kwargs = {"device": "cuda", "dtype": torch.float32, "requires_grad": False}
+    layer_weight = torch.rand((N,), **factory_kwargs)
+    x = -20 + 0.5 * torch.randn((M, N), **factory_kwargs)
+    expected = pytorch_naive_rmsnorm(x, layer_weight, eps)
+
+    # tensors casting
+    layer_weight = layer_weight.to(dtype)
+    x = x.to(dtype)
+
+    fn = implementations_rms_norm[implementation](layer_weight, eps)
     if cuda_graphs:
         run = cuda_graphs_wrapper(model=fn, inputs=[x], copy_outputs=False)
         # cuda graphs wraps output in a tuple
