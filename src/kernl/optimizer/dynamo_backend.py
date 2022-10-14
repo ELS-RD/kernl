62d5b494bb53817489d220992e222619022cb458
Gaetan SENELLE <8348807+gaetansnl@users.noreply.github.com>
2022-10-14 13:40:03 +0200

    feat: add more models (#77)
    * feat: add more models
    
    * feat: onnx test
    
    * fix: reformat
    
    * fix: refactoring
    
    * fix: unused imports
    
    * fix: refactor tests
    
    * fix: review
    
    * fix: rename attention functions
    
    * fix: remove some tests
    
    Co-authored-by: Michaël Benesty <pommedeterresautee@users.noreply.github.com>

--- src/kernl/optimizer/dynamo_backend.py
+++ src/kernl/optimizer/dynamo_backend.py
@@ -15,7 +15,7 @@
 
 import torch
 
-from kernl.optimizer.attention import fuse_attention
+from kernl.optimizer.attention import fuse_attention_pattern_1, fuse_attention_pattern_2
 from kernl.optimizer.dropout import remove_dropout
 from kernl.optimizer.layer_norm import replace_layer_norm
 from kernl.optimizer.linear import replace_all_linear
@@ -25,7 +25,8 @@ from kernl.optimizer.normalizer import normalize_operators
 def dynamo_backend_ofi(gm: torch.fx.GraphModule, assume_causal=False):
     normalize_operators(gm)
     remove_dropout(gm)
-    fuse_attention(gm, assume_causal)
+    fuse_attention_pattern_1(gm, assume_causal)
+    fuse_attention_pattern_2(gm, assume_causal)
     replace_all_linear(gm)
     replace_layer_norm(gm)
     return gm
