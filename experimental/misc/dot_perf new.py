d5155a1ec602d9e5159e64a8222eab99e42f2039
pommedeterresautee <pommedeterresautee@msn.com>
2022-11-28 11:31:12 +0100

    feat: add batch support

--- experimental/misc/dot_perf new.py
+++ experimental/misc/dot_perf new.py
@@ -11,12 +11,15 @@ def kernel(
     Vec,
     Matrix,
     Out,
+    vec_batch_stride,
     vec_n_head_stride,
     vec_seqlen_stride,
     vec_d_head_stride,
+    matrix_batch_stride,
     matrix_n_head_stride,
     matrix_seqlen_stride,
     matrix_d_head_stride,
+    out_batch_stride,
     out_n_head_stride,
     out_seqlen_stride,
     out_d_head_stride,
@@ -24,27 +27,31 @@ def kernel(
     D_HEAD: tl.constexpr,
     ILP: tl.constexpr,
 ):
-    n_block_idx = tl.program_id(0)
+    batch_idx = tl.program_id(0)
     n_head_idx = tl.program_id(1)
+    n_block_idx = tl.program_id(2)
+
     size_n_arange = tl.arange(0, SIZE_N)
     d_head_arange = tl.arange(0, D_HEAD)
 
-    vec_ptr = Vec + n_head_idx * vec_n_head_stride + vec_d_head_stride * d_head_arange[None, :]
+    vec_ptr = Vec + batch_idx * vec_batch_stride + n_head_idx * vec_n_head_stride + vec_d_head_stride * d_head_arange[None, :]
     vec = tl.load(vec_ptr)
 
     for start in range(ILP):
         # transpose matrix
         matrix_ptr = (
             Matrix
+            + batch_idx * matrix_batch_stride
             + n_head_idx * matrix_n_head_stride
             + d_head_arange[None, :] * matrix_d_head_stride
             + ((start + n_block_idx * ILP) * SIZE_N + size_n_arange)[:, None] * matrix_seqlen_stride
         )
         mat = tl.load(matrix_ptr)
         result = mat.to(tl.float32) * vec.to(tl.float32)
-        result = tl.sum(result, axis=1) + start * 0
+        result = tl.sum(result, axis=1)
         out_ptr = (
             Out
+            + batch_idx * out_batch_stride
             + n_head_idx * out_n_head_stride
             + size_n_arange * out_d_head_stride
             + (start + n_block_idx * ILP) * SIZE_N
@@ -52,17 +59,17 @@ def kernel(
         tl.store(out_ptr, result)
 
 
-def triton_wrapper(vec: torch.Tensor, matrix: torch.Tensor, output: torch.Tensor) -> torch.Tensor:
+def triton_wrapper(vec: torch.Tensor, matrix: torch.Tensor, output: torch.Tensor, ilp: int, block_size_n: int) -> torch.Tensor:
     kernel[grid](
         vec,
         matrix,
-        out,
+        output,
         *vec.stride(),
         *matrix.stride(),
-        *out.stride(),
-        size_n,
+        *output.stride(),
+        block_size_n,
         d_head,
-        ILP,
+        ilp,
     )
 
 
@@ -71,18 +78,19 @@ def reference_pytorch(vec: torch.Tensor, matrix: torch.Tensor, output: torch.Ten
 
 
 size_n = 16
-seq_len_k = 1024
+batch_size = 5
 n_head = 20
+seq_len_k = 1024
 d_head = 64
 
-q = torch.randn((n_head, 1, d_head), dtype=torch.float16, device="cuda")
-k = torch.randn((n_head, seq_len_k, d_head), dtype=torch.float16, device="cuda")
-out = torch.zeros((n_head, 1, seq_len_k), dtype=torch.float16, device="cuda")
+q = torch.randn((batch_size, n_head, 1, d_head), dtype=torch.float16, device="cuda")
+k = torch.randn((batch_size, n_head, seq_len_k, d_head), dtype=torch.float16, device="cuda")
+out = torch.zeros((batch_size, n_head, 1, seq_len_k), dtype=torch.float16, device="cuda")
 expected = torch.zeros_like(out)
 
-n_repeat = 10000
+n_repeat = 1000
 ILP = 2
-grid = (triton.cdiv(seq_len_k, size_n * ILP), n_head)
+grid = (batch_size, n_head, triton.cdiv(seq_len_k, size_n * ILP))
 print(grid)
 print("CUDA times")
 
@@ -90,25 +98,25 @@ start_event = [torch.cuda.Event(enable_timing=True) for _ in range(n_repeat)]
 end_event = [torch.cuda.Event(enable_timing=True) for _ in range(n_repeat)]
 # warmup
 for _ in range(n_repeat):
-    triton_wrapper(vec=q, matrix=k, output=out)
+    triton_wrapper(vec=q, matrix=k, output=out, ilp=ILP, block_size_n=size_n)
     reference_pytorch(vec=q, matrix=k, output=expected)
 torch.cuda.synchronize()
+# run PyTorch
 for i in range(n_repeat):
     start_event[i].record()
     reference_pytorch(vec=q, matrix=k, output=expected)
     end_event[i].record()
     torch.cuda.synchronize()
 times_pytorch = torch.median(torch.tensor([s.elapsed_time(e) for s, e in zip(start_event, end_event)]))
-# run
 torch.cuda.synchronize()
+# run Triton
 for i in range(n_repeat):
     start_event[i].record()
-    triton_wrapper(vec=q, matrix=k, output=out)
+    triton_wrapper(vec=q, matrix=k, output=out, ilp=ILP, block_size_n=size_n)
     end_event[i].record()
     torch.cuda.synchronize()
 times_triton = torch.median(torch.tensor([s.elapsed_time(e) for s, e in zip(start_event, end_event)]))
 
-reference_pytorch(vec=q, matrix=k, output=expected)
-assert torch.allclose(out, expected, atol=1e-2), f"{out}\n{expected}"
+assert torch.allclose(out, expected, atol=1e-1), f"{out}\n{expected}"
 print(f"{'tl.sum(a * b, 1)':<20}{times_triton.item() :.9f}")
 print(f"{'vec @ matrix.t()':<20}{times_pytorch.item() :.9f}")
