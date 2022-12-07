24f6dc522088326365599f74c449c4aea7b8663e
pommedeterresautee <pommedeterresautee@msn.com>
2022-12-07 11:06:51 +0100

    fix: assert

--- src/kernl/implementations/attention_vec_mat.py
+++ src/kernl/implementations/attention_vec_mat.py
@@ -162,6 +162,7 @@ def attention_vec_mat_forward(
 ):
     assert is_causal is False, "causal mask is not supported"
     assert attention_mask is None, "attention_mask is not supported"
+    assert output.shape == q.shape[:3] + k.shape[-1:], f"{output.shape} != {q.shape[:3] + k.shape[-1:]}"
     if v.stride()[-1] == 1:  # is row major
         # change layout to col major
         v.set_(source=v.permute(0, 1, 3, 2).contiguous().permute(0, 1, 3, 2))
