5137623a1b5919dd8cc54565176bf92cbfe20346
Vincent Guerin <v.guerin@lefebvre-sarrut.eu>
2022-12-14 14:30:12 +0100

    refactor: charts data loader.

--- docs/javascripts/charts-data-loader.js
+++ docs/javascripts/charts-data-loader.js
@@ -1,4 +1,4 @@
-const buildDefaultData = () => {
+export const buildDefaultData = () => {
     const engines = ['Inductor', 'AITemplate', 'TensorRT', 'DeepSpeed', 'Baseline', 'Cuda graphs', 'Nvfuser', 'Kernl', 'ONNX Runtime'];
     const sequence = [16, 128, 256, 384, 512];
     const batch = [1, 8];
@@ -18,18 +18,14 @@ const buildDefaultData = () => {
     return defaultData;
 }
 
-async function getData() {
+export async function fetchData(url) {
     if (window.fetch) {
         const response = await fetch(url)
-        if (response.ok) {
+        if (response.ok && response.status === 200) {
             return await response.json();
         } else return undefined;
     }
     return undefined;
 }
-
-const url = 'https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits';
-
-export const data = getData(url) || buildDefaultData();
 
 
