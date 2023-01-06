8b0ec7227ec9e7edc96264fcf126dbac1b4b1e64
Michaël Benesty <pommedeterresautee@users.noreply.github.com>
2023-01-06 11:22:11 +0100

    feat: reduce cg memory footprint (#225)

--- test/test_linear_layer.py
+++ test/test_linear_layer.py
@@ -20,8 +20,8 @@ import torch
 
 from conftest import assert_all_close, set_seed
 
-from kernl.implementations.cuda_graph import cuda_graphs_wrapper
 from kernl.implementations.linear_layer import linear_layer
+from kernl.optimizer.cuda_graph import cuda_graphs_wrapper
 
 
 def get_pytorch_activation(activation: str) -> Callable:
