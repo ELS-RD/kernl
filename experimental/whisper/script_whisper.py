fix: precision error

experimental/whisper/script_whisper.py
@@ -1,19 +1,53 @@
 import time
 
 import torch
-import torch._dynamo as torchdynamo
 from datasets import load_dataset
 from transformers import WhisperForConditionalGeneration, WhisperProcessor
 
 from kernl.model_optimization import optimize_model
 
 
-torchdynamo.config.cache_size_limit = 512
 torch.set_float32_matmul_precision("high")
+# torchdynamo.config.cache_size_limit = 512
+# torchdynamo.config.dynamic_shapes = True
+max_len = 50
+num_beams = 5
+model_name = "openai/whisper-large-v2"
 
 audio_dataset = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
+# audio_dataset = load_dataset("librispeech_asr", "clean", split="test")
 
 
+def get_tokens(item: dict[str, dict]) -> torch.Tensor:
+    tensor = processor(item["audio"]["array"], return_tensors="pt", sampling_rate=16_000).input_features
+    return tensor.cuda()
+
+
+processor = WhisperProcessor.from_pretrained(model_name)
+inputs_1 = get_tokens(audio_dataset[0])
+
+model = WhisperForConditionalGeneration.from_pretrained(model_name).to("cuda").eval()
+
+timings_original = list()
+transcriptions = list()
+with torch.inference_mode(), torch.autocast(dtype=torch.float16, cache_enabled=True, device_type="cuda"):
+    # warmup
+    model.generate(inputs_1, min_length=max_len, max_length=max_len, num_beams=num_beams, do_sample=False)
+    torch.cuda.synchronize()
+    for audio in audio_dataset:
+        inputs = get_tokens(audio)
+        torch.cuda.synchronize()
+        start = time.time()
+        predicted_ids = model.generate(inputs, min_length=1, max_length=max_len, num_beams=num_beams, do_sample=False)
+        torch.cuda.synchronize()
+        timings_original.append(time.time() - start)
+        transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True, normalize=True)[0]
+        transcriptions.append(transcription)
+
+assert len(audio_dataset) == len(transcriptions)
+
+
+# apply efficiency fix to HuggingFace implementation of whisper to limit memory footprint
 @staticmethod
 def fix_reorder_cache(past, beam_idx):
     reordered_past = ()
@@ -26,54 +60,51 @@ def fix_reorder_cache(past, beam_idx):
 
 WhisperForConditionalGeneration._reorder_cache = fix_reorder_cache
 
-
-model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-tiny").to("cuda").eval()
-
 model.model.decoder.forward_before = model.model.decoder.forward
 
-
-def wrapper_stride(*args, **kwargs):
-    o = model.model.decoder.forward_before(*args, **kwargs)
-    past_key_values = list(o.past_key_values)
-    for idx in range(len(past_key_values)):
-        layer = past_key_values[idx]
-        v = layer[-1]
-        if v.stride(2) != 1:
-            v_col_major = v.permute(0, 1, 3, 2).contiguous().permute(0, 1, 3, 2)
-            past_key_values[idx] = layer[:-1] + (v_col_major,)
-
-    o.past_key_values = tuple(past_key_values)
-
-    return o
-
-
-model.model.decoder.forward = wrapper_stride
-
-
 optimize_model(model.model.decoder)
-
-processor = WhisperProcessor.from_pretrained("openai/whisper-tiny")
-
-max_len = 50
-timing = list()
-inputs_1 = processor(audio_dataset[0]["audio"]["array"], return_tensors="pt", sampling_rate=16_000).input_features.to(
-    "cuda"
-)
+nb_diff = 0
+timings_optimized = list()
+print("difference between original and optimized model:")
 with torch.inference_mode(), torch.autocast(dtype=torch.float16, cache_enabled=True, device_type="cuda"):
-    model.generate(inputs_1, min_length=max_len, max_length=max_len, num_beams=5, do_sample=False)
+    start = time.time()
+    model.generate(inputs_1, min_length=max_len, max_length=max_len, num_beams=num_beams, do_sample=False)
     torch.cuda.synchronize()
-    for audio in audio_dataset:
-        inputs = processor(audio["audio"]["array"], return_tensors="pt", sampling_rate=16_000).input_features.to("cuda")
+    print(f"time to warmup: {time.time() - start:.2f}s")
+    for original_modem_transcription, audio in zip(transcriptions, audio_dataset):
+        inputs = get_tokens(audio)
         torch.cuda.synchronize()
         start = time.time()
-        predicted_ids = model.generate(inputs, min_length=1, max_length=max_len, num_beams=5, do_sample=False)
+        predicted_ids = model.generate(inputs, min_length=1, max_length=max_len, num_beams=num_beams, do_sample=False)
         torch.cuda.synchronize()
-        timing.append(time.time() - start)
-        print(time.time() - start)
-        transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True, normalize=True)[0]
-        print(transcription)
+        timings_optimized.append(time.time() - start)
+        optimized_transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True, normalize=True)[0]
+        nb_diff += original_modem_transcription != optimized_transcription
 
-print(f"Average time: {sum(timing) / len(timing)}", f"Complete time: {sum(timing)}")
+print("timings")
+print(f"[original] average: {sum(timings_original) / len(timings_original)}s / complete: {sum(timings_original)}s")
+print(f"[optimized] average: {sum(timings_optimized) / len(timings_optimized)}s / complete: {sum(timings_optimized)}s")
+print(f"output differences: {nb_diff}/{len(audio_dataset)} ({nb_diff / len(audio_dataset) * 100:.2f}%)")
+
+print("memory footprint")
 print("torch.cuda.memory_allocated: %fGB" % (torch.cuda.memory_allocated(0) / 1024 / 1024 / 1024))
 print("torch.cuda.memory_reserved: %fGB" % (torch.cuda.memory_reserved(0) / 1024 / 1024 / 1024))
 print("torch.cuda.max_memory_reserved: %fGB" % (torch.cuda.max_memory_reserved(0) / 1024 / 1024 / 1024))
+
+# before modification without stride fix
+# difference between original and optimzed model:
+# optimized model modifies 31.83% (# 834 examples) of the dataset
+# [original] average: 1.0774035485646196s / complete: 2822.7972972393036s
+# [optimized] average: 0.45561475826583747s / complete: 1193.7106666564941s
+# torch.cuda.memory_allocated: 10.874530GB
+# torch.cuda.memory_reserved: 14.064453GB
+# torch.cuda.max_memory_reserved: 14.064453GB
+# after modification
+# difference between original and optimzed model:
+# time to warmup: 694.1382637023926
+# optimized model modifies 2.06% (# 54/2620 examples)
+# [original] average: 1.0491925114893732s / complete: 2748.8843801021576s
+# [optimized] average: 0.4339728889574531s / complete: 1137.0089690685272s
+# torch.cuda.memory_allocated: 10.873960GB
+# torch.cuda.memory_reserved: 13.365234GB
+# torch.cuda.max_memory_reserved: 13.853516GB

src/kernl/implementations/attention.py
@@ -101,15 +101,15 @@ def prune(configs, named_args):
         triton.Config({"BLOCK_M_SIZE": 128, "BLOCK_N_SIZE": 32}, num_stages=1, num_warps=4),
         triton.Config({"BLOCK_M_SIZE": 128, "BLOCK_N_SIZE": 64}, num_stages=1, num_warps=4),
         triton.Config({"BLOCK_M_SIZE": 128, "BLOCK_N_SIZE": 128}, num_stages=1, num_warps=4),
-        triton.Config({"BLOCK_M_SIZE": 128, "BLOCK_N_SIZE": 128}, num_stages=1, num_warps=8),
+        # triton.Config({"BLOCK_M_SIZE": 128, "BLOCK_N_SIZE": 128}, num_stages=1, num_warps=8),
         # triton.Config({"BLOCK_M_SIZE": 128, "BLOCK_N_SIZE": 256}, num_stages=1, num_warps=8),
         # triton.Config({"BLOCK_M_SIZE": 256, "BLOCK_N_SIZE": 128}, num_stages=1, num_warps=8),
         # triton.Config({"BLOCK_M_SIZE": 256, "BLOCK_N_SIZE": 256}, num_stages=1, num_warps=16),
     ],
     prune_configs_by={"early_config_prune": prune, "perf_model": None, "top_k": None},
-    key=["cache_key_m_size", "cache_key_n_size", "head_size", "HAS_MASK", "IS_MATRIX_MASK", "IS_CAUSAL"],
+    key=["m_size", "n_size", "head_size"],
 )
-@triton.heuristics(  # order should be the same than in function args, otherwise expect strange bugs
+@triton.heuristics(  # order should be the same as in function args, otherwise expect strange bugs
     {
         # load mask is needed if one dim (n_size / m_size) of tensors do not align with block size
         "M_LOAD_MASK_NEEDED": lambda args: args["m_size"] % args["BLOCK_M_SIZE"] != 0,
@@ -121,8 +121,6 @@ def _fwd_kernel(
     head_size,
     m_size,
     n_size,
-    cache_key_m_size,
-    cache_key_n_size,
     q_ptr,
     k_ptr,
     v_ptr,
@@ -507,8 +505,6 @@ class Attention(torch.autograd.Function):
             head_size,  # heads
             m_size,  # m_size
             n_size,  # n_size
-            m_size // 32,  # cache_key_m_size
-            n_size // 32,  # cache_key_n_size
             q,  # Q
             k,  # K
             v,  # V