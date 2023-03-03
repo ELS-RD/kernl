00a384c6b3e075aff7ec4a939f9c62ee3511aaf1
pommedeterresautee <pommedeterresautee@msn.com>
2023-03-03 09:56:54 +0100

    feat: add load mask

--- modified_original_reworked.py
+++ modified_original_reworked.py
@@ -11,56 +11,54 @@ import triton
 import triton.language as tl
 
 
-def generate_broadcast_mask(
-    batch: int, heads: int, seq_length: int, dhead: int, dtype: torch.Tensor = torch.float32
-) -> torch.Tensor:
+def generate_broadcast_mask(batch: int, heads: int, seq_length: int, dhead: int) -> torch.Tensor:
     attention_mask = (
-        torch.randint(1, seq_length, (batch,), device="cuda")[:, None]
-        > torch.arange(0, seq_length, device="cuda")[None, :]
+            torch.randint(1, seq_length, (batch,), device="cuda")[:, None]
+            > torch.arange(0, seq_length, device="cuda")[None, :]
     )
     attention_mask = torch.reshape(attention_mask, (batch, 1, 1, seq_length))
     attention_mask = torch.where(attention_mask, 0, float("-inf"))
-    attention_mask = attention_mask.to(dtype)
-    # attention_mask = (1.0 - attention_mask) * torch.finfo(dtype).min
+    assert attention_mask.dtype == torch.float32
     return attention_mask
 
 
 @triton.jit
 def _fwd_kernel(
-    Q,
-    K,
-    V,
-    sm_scale,
-    attention_mask_ptr,
-    L,
-    M,
-    Out,
-    stride_qz,
-    stride_qh,
-    stride_qm,
-    stride_qk,
-    stride_kz,
-    stride_kh,
-    stride_kn,
-    stride_kk,
-    stride_vz,
-    stride_vh,
-    stride_vk,
-    stride_vn,
-    stride_oz,
-    stride_oh,
-    stride_om,
-    stride_on,
-    attention_mask_batch_stride,
-    attention_mask_head_stride,
-    attention_mask_m_stride,
-    attention_mask_n_stride,
-    Z,
-    H,
-    N_CTX,
-    BLOCK_M: tl.constexpr,
-    BLOCK_DMODEL: tl.constexpr,
-    BLOCK_N: tl.constexpr,
+        Q,
+        K,
+        V,
+        sm_scale,
+        attention_mask_ptr,
+        L,
+        M,
+        Out,
+        stride_qz,
+        stride_qh,
+        stride_qm,
+        stride_qk,
+        stride_kz,
+        stride_kh,
+        stride_kn,
+        stride_kk,
+        stride_vz,
+        stride_vh,
+        stride_vk,
+        stride_vn,
+        stride_oz,
+        stride_oh,
+        stride_om,
+        stride_on,
+        attention_mask_batch_stride,
+        attention_mask_head_stride,
+        attention_mask_m_stride,
+        attention_mask_n_stride,
+        Z,
+        H,
+        N_CTX,
+        BLOCK_M: tl.constexpr,
+        BLOCK_DMODEL: tl.constexpr,
+        BLOCK_N: tl.constexpr,
+        USE_MASK: tl.constexpr,
 ):
     start_m = tl.program_id(0)
     off_hz = tl.program_id(1)
@@ -81,22 +79,28 @@ def _fwd_kernel(
     l_prev = tl.zeros([BLOCK_M], dtype=tl.float32)
     acc = tl.zeros([BLOCK_M, BLOCK_DMODEL], dtype=tl.float32)
     # load q: it will stay in SRAM throughout
-    q = tl.load(q_ptrs)
+    # q = tl.load(q_ptrs)
+    q = tl.load(q_ptrs, mask=offs_m[:, None] < N_CTX, other=0.0)
     # loop over k, v and update accumulator
     for start_n in range(0, N_CTX, BLOCK_N):
+        block_n_offs = start_n + offs_n
         # -- compute qk ----
-        k = tl.load(k_ptrs)
+        # k = tl.load(k_ptrs)
+        k = tl.load(k_ptrs, mask=block_n_offs[:, None] < N_CTX, other=0.0)
         qk = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32)
         qk += tl.dot(q, tl.trans(k))
         qk *= sm_scale
         # qk = tl.where(offs_m[:, None] >= (start_n + offs_n[None, :]), qk, float("-inf"))  # no causal mask
         # fake mask
-        attention_mask_offs = off_attention + offs_n[None, :] * attention_mask_n_stride
-        attention_mask = tl.load(
-            attention_mask_ptr + attention_mask_offs,
-            eviction_policy="evict_first",
-        )
-        qk = tl.where(attention_mask, qk, float("-inf"))  # no causal mask
+        if USE_MASK:
+            attention_mask_offs = off_attention + offs_n[None, :] * attention_mask_n_stride
+            attention_mask_ptr_mask = block_n_offs[None, :] < N_CTX
+            attention_mask = tl.load(attention_mask_ptr + attention_mask_offs,
+                                     eviction_policy="evict_first",
+                                     mask=attention_mask_ptr_mask,
+                                     other=float("-inf")
+                                     )
+            qk = tl.where(attention_mask, qk, float("-inf"))  # no causal mask
         # compute new m
         m_curr = tl.maximum(tl.max(qk, 1), m_prev)
         # correct old l
@@ -110,7 +114,9 @@ def _fwd_kernel(
         acc *= (l_prev * l_rcp)[:, None]
         # update acc
         p = p.to(tl.float16)
-        v = tl.load(v_ptrs)
+        # v = tl.load(v_ptrs)
+        v = tl.load(v_ptrs, mask=block_n_offs[:, None] < N_CTX, other=0.0)
+
         acc += tl.dot(p, v)
         # update m_i and l_i
         l_prev = l_curr
@@ -130,7 +136,8 @@ def _fwd_kernel(
     offs_n = tl.arange(0, BLOCK_DMODEL)
     off_o = off_hz * stride_oh + offs_m[:, None] * stride_om + offs_n[None, :] * stride_on
     out_ptrs = Out + off_o
-    tl.store(out_ptrs, acc)
+    # tl.store(out_ptrs, acc)
+    tl.store(out_ptrs, acc, mask=offs_m[:, None] < N_CTX)
 
 
 class _attention(torch.autograd.Function):
@@ -145,76 +152,67 @@ class _attention(torch.autograd.Function):
         grid = (triton.cdiv(q.shape[2], BLOCK), q.shape[0] * q.shape[1], 1)
         L = torch.empty((q.shape[0] * q.shape[1], q.shape[2]), device=q.device, dtype=torch.float32)
         m = torch.empty((q.shape[0] * q.shape[1], q.shape[2]), device=q.device, dtype=torch.float32)
-        num_warps = 4 if Lk <= 64 else 8
 
         _fwd_kernel[grid](
             q,
             k,
             v,
             sm_scale,
-            attention_mask,
+            attention_mask if attention_mask is not None else q,
             L,
             m,
             o,
-            q.stride(0),
-            q.stride(1),
-            q.stride(2),
-            q.stride(3),
-            k.stride(0),
-            k.stride(1),
-            k.stride(2),
-            k.stride(3),
-            v.stride(0),
-            v.stride(1),
-            v.stride(2),
-            v.stride(3),
-            o.stride(0),
-            o.stride(1),
-            o.stride(2),
-            o.stride(3),
-            *attention_mask.stride(),
-            q.shape[0],
-            q.shape[1],
-            q.shape[2],
+            *q.stride(),
+            *k.stride(),
+            *v.stride(),
+            *o.stride(),
+            *attention_mask.stride() if attention_mask is not None else q.stride(),
+            Z=q.shape[0],
+            H=q.shape[1],
+            N_CTX=q.shape[2],
             BLOCK_M=BLOCK,
             BLOCK_N=BLOCK,
             BLOCK_DMODEL=Lk,
-            num_warps=num_warps,
+            USE_MASK=attention_mask is not None,
+            num_warps=4 if Lk <= 64 else 8,
             num_stages=2,
         )
         # print(h.asm["ttgir"])
 
-        ctx.save_for_backward(q, k, v, o, L, m)
-        ctx.grid = grid
-        ctx.sm_scale = sm_scale
-        ctx.BLOCK_DMODEL = Lk
         return o
 
 
 attention = _attention.apply
 
 
-@pytest.mark.parametrize("Z, H, N_CTX, D_HEAD", [(4, 48, 128, 64), (4, 48, 512, 64)])
-def test_op(Z, H, N_CTX, D_HEAD, dtype=torch.float16):
+@pytest.mark.parametrize(
+    "Z, H, N_CTX, D_HEAD, use_mask",
+    [
+        (4, 48, 128, 64, True),
+        (4, 48, 512, 64, True),
+        (4, 48, 512, 64, False),
+        (4, 48, 65, 64, False),
+        (4, 48, 65, 64, True)
+    ],
+)
+def test_op(Z, H, N_CTX, D_HEAD, use_mask):
     torch.manual_seed(20)
-    q = torch.rand((Z, H, N_CTX, D_HEAD), dtype=dtype, device="cuda")
-    k = torch.rand((Z, H, N_CTX, D_HEAD), dtype=dtype, device="cuda")
-    v = torch.rand((Z, H, N_CTX, D_HEAD), dtype=dtype, device="cuda")
+    q = torch.rand((Z, H, N_CTX, D_HEAD), dtype=torch.float16, device="cuda")
+    k = torch.rand((Z, H, N_CTX, D_HEAD), dtype=torch.float16, device="cuda")
+    v = torch.rand((Z, H, N_CTX, D_HEAD), dtype=torch.float16, device="cuda")
     sm_scale = 0.3
     # reference implementation
     p = torch.matmul(q, k.transpose(2, 3)) * sm_scale
-    # no causal mask
-    # M = torch.tril(torch.ones((N_CTX, N_CTX), device="cuda"))
-    # p = torch.where(M == 0, float("-inf"), p)
-    # fake mask
-    fake_attention_mask = generate_broadcast_mask(batch=Z, heads=H, seq_length=N_CTX, dhead=D_HEAD, dtype=torch.float32)
+    # (not causal) attention mask to be broadcasted
+    if use_mask:
+        # shape: batch, 1, 1, seq_length
+        fake_attention_mask = generate_broadcast_mask(batch=Z, heads=H, seq_length=N_CTX, dhead=D_HEAD)
+        p += fake_attention_mask
+    else:
+        fake_attention_mask = None
 
-    p += fake_attention_mask
     p = torch.softmax(p.float(), dim=-1).half()
     ref_out = torch.matmul(p, v)
-
-    # triton implementation
     tri_out = attention(q, k, v, sm_scale, fake_attention_mask)
     # compare
-    print((ref_out - tri_out).abs().max().item())
     triton.testing.assert_almost_equal(ref_out, tri_out, decimal=1)
